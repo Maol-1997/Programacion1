0142b36063aa257e539a35d518b76852c6be4531
Maol-1997 <42623358+Maol-1997@users.noreply.github.com>
2018-09-06 08:55:53 -0300

    ej 7
    subiendo tp ej 7

---
+++ tp1/ej 7.php
@@ -0,0 +1,26 @@
+<?php
+	$a=0;
+	$xd=0;
+	do{
+		$arreglo[$a] = random_int(1,30);
+		$a++;
+	}while($a-1 < 10 || $arreglo[$a-1] != 30);
+	asort($arreglo);
+	$b =(int)(count($arreglo)/2);
+	foreach($arreglo as $val){
+		$xd++;
+		if($xd <= $b)
+			$array1[$xd] = $val;
+		else
+			$array2[$xd] = $val;
+	}
+	echo "Primer array:<br>";
+	foreach($array1 as $val1){
+		echo $val1." ";
+	}
+	echo "<br>Segundo array:<br>";
+	foreach($array2 as $val2){
+		echo $val2." ";
+	}
+	
+?>
