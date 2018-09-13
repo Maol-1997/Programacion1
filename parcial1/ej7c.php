89db59733eecf3713c1c953fa5a468e22068cbfd
Maol-1997 <42623358+Maol-1997@users.noreply.github.com>
2018-09-13 11:03:06 -0300

    fix

--- parcial1/ej7c.php
+++ parcial1/ej7c.php
@@ -1,18 +1,14 @@
-<table>
+
 <?php
-$cantidad =  (int)(count($_GET)/2);
-if($_GET % 2 != 0) {
-    for ($i = 0; $i < $cantidad; $i++) {
-        echo "<tr>";
-        for ($e = 0; $e < $cantidad; $e++) {
-            $posicion = $e + $cantidad * $i;
-            if (isset($_GET["numero" . $posicion]))
-                echo "<td>" . $_GET["numero" . $posicion] . "</td>";
-        }
-        echo "</tr>";
-    }
-} else {
-    echo "<td>" . array_values(array_slice($_GET, -1))[0] . "</td>";
+$cantidad =  (count($_GET)/2);
+$array = (array_chunk($_GET,ceil($cantidad)));
+foreach ($array[0] as $valor){
+    echo $valor." ";
 }
+echo "<br>";
+foreach ($array[1] as $valor){
+    echo $valor." ";
+}
+echo "</br>";
 ?>
 </table>
